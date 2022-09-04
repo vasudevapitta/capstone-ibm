91c1c02ab9f791fe22ce7ba18c88dc495548d653
Vasudeva Pitta <tizzicboy@gmail.com>
2022-09-04 23:13:05 +0000

    deploy app on ibm cloud

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ['vasuana.us-east.cf.appdomain.cloud', 'localhost', 'localhost:8000', '127.0.0.1', '*']
 
 
 # Application definition
