35437e80fe8cdb7cca76ef6807800c398806609b
Vasudeva Pitta <tizzicboy@gmail.com>
2022-09-09 19:01:43 +0000

    import models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 
 # from .models import related models
-from .models import CarModel, CarMake, CarDealer
+from .models import CarMake, CarModel, CarDealer, DealerReview
 
 # from .restapis import related methods
 from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf
